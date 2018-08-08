40571cdceeb21834e364dadf849edf3228200d90
Ramu Kithada <ramu.kithada@gmail.com>
2018-08-08 11:19:07 +0530

    card activity

---
+++ app/src/main/java/com/prachatech/appointment/activity/CardRegisterActivity.java
@@ -0,0 +1,16 @@
+package com.prachatech.appointment.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.prachatech.appointment.R;
+
+public class CardRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_register);
+
+    }
+}
